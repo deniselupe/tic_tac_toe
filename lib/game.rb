6207456e2e7aadbb8e20a475b3f9b0025ca61cfa
deniselupe <dgrodriguez24@hotmail.com>
2022-01-14 22:25:59 +0000

    feat: locate winner and determine if board is full

--- lib/game.rb
+++ lib/game.rb
@@ -7,8 +7,8 @@ class Game
     @board_axis_size = 3
     @game_pieces = %w[X O]
     @win_condition = [
-      [1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7],
-      [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]
+      [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6],
+      [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]
     ]
     @players = []
     @current_player = 0
@@ -16,30 +16,6 @@ class Game
 
   def add_player(player)
     @players.push(player) if @players.length < 2
-  end
-
-  def player_list
-    @players.each { |player| puts "Player: #{player.name}" }
-  end
-
-  def toggle_player
-    case @current_player
-    when 0 then @current_player = 1
-    when 1 then @current_player = 0
-    end
-  end
-
-  def select_position(player)
-    puts "\n#{player.name}, choose a position from the available positions on the board:"
-    position = gets.chomp.to_i - 1
-
-    until @board[position].instance_of?(Integer)
-      puts "\nThis position is not available. Please choose again:"
-      position = gets.chomp.to_i - 1
-    end
-
-    @board[position] = player.game_piece
-    print_board
   end
 
   def player_info
@@ -71,11 +47,45 @@ class Game
     end
   end
 
+  def player_list
+    @players.each { |player| puts "Player: #{player.name}" }
+  end
+
   def print_board
     puts "\n #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts '---|---|---'
     puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
     puts '---|---|---'
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} \n"
+  end
+
+  def toggle_player
+    case @current_player
+    when 0 then @current_player = 1
+    when 1 then @current_player = 0
+    end
+  end
+
+  def select_position(player)
+    puts "\n#{player.name}, choose a position from the available positions on the board:"
+    position = gets.chomp.to_i - 1
+
+    until @board[position].instance_of?(Integer)
+      puts "\nThis position is not available. Please choose again:"
+      position = gets.chomp.to_i - 1
+    end
+
+    @board[position] = player.game_piece
+    print_board
+  end
+
+  def winner?(player)
+    @win_condition.any? do |condition|
+      condition.all? { |position| @board[position] == player.game_piece }
+    end
+  end
+
+  def board_full?
+    @board.all? { |position| position.instance_of?(String) }
   end
 end
